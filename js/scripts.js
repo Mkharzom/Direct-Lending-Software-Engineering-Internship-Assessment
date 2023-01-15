e23966c61d1a23e0e467e461228d374cbd875786
Mkharzom <76581486+Mkharzom@users.noreply.github.com>
2023-01-15 10:01:47 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ function getState() {
                 }
                 catch(err){
                     document.getElementById("state").value = "Postcode not found";
+                    alert('check your Postcode');
                 }
             } 
         }
