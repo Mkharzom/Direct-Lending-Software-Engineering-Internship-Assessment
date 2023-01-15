d3971a1077a7cc8682d69ed9a97d77b1ed807ddc
Mkharzom <76581486+Mkharzom@users.noreply.github.com>
2023-01-15 09:24:34 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,59 @@
 function getState() {
-    const testd = document.getElementById("postcode").value;
-        if(testd == 53100){
-            document.getElementById("state").value="Taman Melwati";
+    // Get the postcode entered by the user
+    var postcode = document.getElementById("postcode").value;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "php/get_state.php?postcode="+ postcode, true);
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                try{
+                    var data = JSON.parse(xhr.responseText);
+                    if(data.status === "success"){
+                        document.getElementById("state").value = data.state;
+                    }
+                }
+                catch(err){
+                    document.getElementById("state").value = "Postcode not found";
+                }
+            } 
         }
-        else{
-            document.getElementById("state").value="ERROR";
-        }
-  }
+        
+    };
+    
+    xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+    };
+    xhr.send(null);
+}
+
+  // JavaScript function to handle form submission
+function submitForm() {
+    // Get form data
+    document.getElementById("state").removeAttribute("disabled");
+    var formData = {
+        'name': $('input[name=name]').val(),
+        'dob': $('input[name=dob]').val(),
+        'address': $('input[name=address]').val(),
+        'postcode': $('input[name=postcode]').val(),
+        'state': $('input[name=state]').val()
+    };
+
+    // Make an AJAX call to the "Submit/Save" endpoint
+    $.ajax({
+        type: 'POST',
+        url: 'php/submit.php',
+        data: formData,
+        dataType: 'json',
+        encode: true
+    })
+    .done(function(data) {
+        // Handle successful submission
+        console.log(data);
+        alert("Data submitted successfully");
+    })
+    .fail(function(data) {
+        // Handle error
+        console.log('Error:', data);
+        alert("Error submitting data");
+    });
+}
